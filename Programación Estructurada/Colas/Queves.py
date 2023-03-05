84483b0b982d021fc6ada410c363593649f28609
Mario Aguilar Real <94491813+MarioAguilarReal@users.noreply.github.com>
2023-03-04 17:28:27 -0800

    Ediciones

--- Programación Estructurada/Colas/Queves.py
+++ Programación Estructurada/Colas/Queves.py
@@ -2,6 +2,8 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+        self.prev = None
+        self.down = None
     
     def __str__(self):
         return str(self.value)
